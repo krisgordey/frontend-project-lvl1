a4c5faba0720cd47717449d4f563e0b5071b8bf8
Kristina Gordeeva <krisgordeeva@gmail.com>
2020-05-12 22:59:30 +0300

    add linter

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 import start from '../src/cli.js';
+
 console.log('Welcome to the Brain Games!');
 start();
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export default function () {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log('Hi ' + userName + '!');
-};
-
+export default function question() {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hi ${userName}!`);
+}
